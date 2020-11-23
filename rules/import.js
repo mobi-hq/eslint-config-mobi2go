6754cf38e9d973da2b1d351827e2dbbd0c1a635c
Nils Citoleux <nils@mobi2go.com>
2020-11-23 11:32:17 +0100

    Fix import rules to support typescript

--- rules/import.js
+++ rules/import.js
@@ -1,9 +1,24 @@
 module.exports = {
-    plugins: [
-        'import',
-    ],
+    plugins: ['import'],
     rules: {
         'import/no-extraneous-dependencies': 'error',
         'import/namespace': ['error', { allowComputed: true }],
-    }
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            {
+                js: 'never',
+                jsx: 'never',
+                ts: 'never',
+                tsx: 'never',
+            },
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+            },
+        },
+    },
 };
